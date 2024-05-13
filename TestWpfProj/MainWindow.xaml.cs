4a492d99eba59c384b986bbf800b78b146dd17b7
HELLO MILKRIZU <milkrizu@mail.ru>
2024-05-13 16:49:59 +0300

    сортировка

--- TestWpfProj/MainWindow.xaml.cs
+++ TestWpfProj/MainWindow.xaml.cs
@@ -30,6 +30,11 @@ namespace TestWpfProj
             _memes = Data.DataContext.Memes;
             _memeTypes = Data.DataContext.MemeTypes;
 
+            foreach (var meme in _memes)
+            {
+                meme.Price = meme.Title.Length * 2;  
+            }
+
             LstView.ItemsSource = _memes;
             FilterCB.ItemsSource = _memeTypes;
         }
@@ -59,9 +64,9 @@ namespace TestWpfProj
         {
             var tempLst = _memes;
 
-            if(!String.IsNullOrEmpty(SerchTB.Text))
+            if (!String.IsNullOrEmpty(SerchTB.Text))
             {
-                tempLst = _memes.Where(x => x.Title.Contains(SerchTB.Text)).ToList();
+                tempLst = _memes.Where(x => x.Title.Contains(SerchTB.Text) || x.Id.StartsWith(SerchTB.Text)).ToList();
             }
 
             LstView.ItemsSource = tempLst;
@@ -94,6 +99,30 @@ namespace TestWpfProj
             //filter = filter.Where(x => x.MemeType.Id == type.Id).ToList();
             //LstView.ItemsSource = filter;
             //LstView.Items.Refresh();
+        }
+
+        private void SortCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedSortOption = (ComboBoxItem)SortCB.SelectedItem;
+
+            switch (selectedSortOption.Content.ToString())
+            {
+                case "Sort A-Я":
+                    _memes = _memes.OrderBy(x => x.Title).ToList();
+                    break;
+                case "Sort Я-А":
+                    _memes = _memes.OrderByDescending(x => x.Title).ToList();
+                    break;
+                case "Sort Price Asc":
+                    _memes = _memes.OrderBy(x => x.Price).ToList();
+                    break;
+                case "Sort Price Desc":
+                    _memes = _memes.OrderByDescending(x => x.Price).ToList();
+                    break;
+            }
+
+            LstView.ItemsSource = _memes;
+            LstView.Items.Refresh();
         }
     }
 }
