f4125d689b34c19189b961db2d129382a96d0c41
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2024-05-17 17:48:12 +0300

    запонил тестовые данные

--- TestWpfProj/Data/DataContext.cs
+++ TestWpfProj/Data/DataContext.cs
@@ -12,7 +12,8 @@ namespace TestWpfProj.Data
         {
             new MemeType("Animals memes"),
             new MemeType("Tit-tok memes"),
-            new MemeType("Skufs memes")
+            new MemeType("Skufs memes"),
+            new MemeType("mebel")
         };
 
         public static List<Meme> Memes = new List<Meme>()
@@ -21,7 +22,13 @@ namespace TestWpfProj.Data
             new Meme("bruh", MemeTypes[1], 99),
             new Meme("Omsk", MemeTypes[2], 199),
             new Meme("MalishkaHochetSdatPraktiku", MemeTypes[0], 69),
-            new Meme("Sigma", MemeTypes[1], 119)
+            new Meme("Sigma", MemeTypes[1], 119),
+            new Meme("Stul", MemeTypes[3], 100),
+            new Meme("stol", MemeTypes[3], 300),
+            new Meme("hkaf", MemeTypes[3], 500),
+            new Meme("kover", MemeTypes[3], 250),
+            new Meme("kreslo", MemeTypes[3], 400),
+            new Meme("divan", MemeTypes[3], 1000)
         };
     }
 }
